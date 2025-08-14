ee4220ddda56c3bcb5e91300989a4ca259d16e18
luqastw <lucasborcem1@gmail.com>
2025-08-14 19:48:17 -0300

    fix: mudança no note3 e .gitignore

--- study/cli/note3.py
+++ study/cli/note3.py
@@ -1,10 +1,10 @@
 # Estudo N°3 da biblioteca argparse.
 
 import argparse
-parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+parser = argparse.ArgumentParser(prog="lqspow", description="calculate X to the power of Y using %(prog)s")
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
+group.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+group.add_argument("-q", "--quiet", action="store_true", help="run simple output")
 parser.add_argument("x", type=int, help="the base")
 parser.add_argument("y", type=int, help="the exponent")
 args = parser.parse_args()
